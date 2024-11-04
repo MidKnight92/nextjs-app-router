9aebfd5ad56e2f78c34e63c73b77fbbb52bf50ab
Jim Cooper <jim@cooperplanet.com>
2024-11-04 12:37:53 -0700

    Cleanup

--- _course-resources/home/home.jsx
+++ _course-resources/home/home.jsx
@@ -1,10 +1,10 @@
 <>
   <div className={styles.heroImage}>
-    <Image src={heroBanner} layout="fill" objectFit="cover" />
+    <Image src={heroBanner} fill alt="" />
   </div>
   <div className={styles.cta}>
-    <Image style={{ marginLeft: '10px' }} height=" 200" width="200" layout="fixed" src={divinity} alt="" />
-    <Image height="200" width="200" layout="fixed" src={synchronicity} alt="" />
+    <Image style={{ marginLeft: '10px' }} height=" 200" width="200" src={divinity} alt="" />
+    <Image height="200" width="200" src={synchronicity} alt="" />
     <div className={styles.ctaText}>
       <div>
         Your <em>Album</em> and <em>Artist</em> Name
@@ -14,10 +14,10 @@
       </div>
       <button className="buton cta">Shop Now</button>
     </div>
-    <Image height="200" width="200" layout="fixed" src={theUndertaking} alt="" />
-    <Image height="200" width="200" layout="fixed" src={asTheSunSpeaks} alt="" />
+    <Image height="200" width="200" src={theUndertaking} alt="" />
+    <Image height="200" width="200" src={asTheSunSpeaks} alt="" />
   </div>
   <div className={styles.soundwavesImage}>
-    <Image src={soundwaves} layout="responsive" alt='' />
+    <Image src={soundwaves} alt='' />
   </div>
 </>
